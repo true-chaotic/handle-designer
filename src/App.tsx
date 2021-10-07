d2d80a86948c5c9c318e2bc7deb91db19822a729
Samokhin Alex <truechaoticphone@gmail.com>
2021-10-07 20:59:25 +0300

    remove extra files, add save button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import React, {FC, useEffect} from 'react';
+import React, {FC, useEffect, useCallback} from 'react';
+import ReactDOM from "react-dom";
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container'
+import Button from '@mui/material/Button';
 import * as paper from 'paper/dist/paper-core';
 import './app.scss';
 import drawHandle from "./drawHandle";
@@ -16,6 +19,10 @@ const App: FC = () => {
         drawHandle();
     });
 
+    const downLoadSvg = useCallback(() => {
+        console.log(paper.project.exportSVG({asString: true}));
+    }, [paper]);
+
     return (
         <div>
             <AppBar position="static">
@@ -28,8 +35,11 @@ const App: FC = () => {
             <div style={{overflow: 'auto'}}>
                 <canvas id="myCanvas" width="500" height="500"/>
             </div>
+            <Container>
+                <Button variant="contained" size="large" onClick={downLoadSvg}>Download svg</Button>
+            </Container>
         </div>
     );
 };
 
-export default App;
+ReactDOM.render(<App />, document.getElementById("app"));
