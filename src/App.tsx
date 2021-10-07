0f2cf5fedd121814e73d8f4ff28a9f20fb293f69
Samokhin Alex <truechaoticphone@gmail.com>
2021-10-07 13:32:11 +0300

    add MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import React, { FC } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import './app.scss';
 import './paper';
 
 const App: FC = () => (
-    <div>&nbsp;</div>
+    <div>
+        <AppBar position="static">
+            <Toolbar variant="dense">
+                <Typography variant="h6" color="inherit" component="div">
+                    Hurdy-Gurdy handle designer
+                </Typography>
+            </Toolbar>
+        </AppBar>
+        <canvas id="myCanvas" width="500" height="500" />
+    </div>
 );
 
 export default App;
